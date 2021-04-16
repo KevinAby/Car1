0676671f0a8a33502b7a5ab3f63c79dd7c61de1e
KevinAby <78090655+KevinAby@users.noreply.github.com>
2021-04-16 16:04:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var car
+var canvas 
+var gameState=0
+var BackImg
+var playerCount
+var form,player,game;
+function setup() {
+  
+ canvas =  createCanvas(400, 400);
+
+//car= createSprite(400,300,40,40)
+
+}
+
+function draw() {
+  background(10);
+
+      drawSprites();
+}
